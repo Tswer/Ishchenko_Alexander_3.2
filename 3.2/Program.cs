326910c94624a42c0cc3c8edf81c502e6e085eeb
Tswer <66019739+Tswer@users.noreply.github.com>
2020-05-27 18:55:44 +0300

    Lab3.2

---
+++ 3.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>()
+            {
+                { 1, "Ukraine" },
+                { 2, "Russia"},
+                { 3, "Poland"},
+                { 4, "USA"}
+            };
+            Dictionary<int, string> d1 = new Dictionary<int, string>();
+            Dictionary<int, string> d2 = new Dictionary<int, string>();
+            Console.WriteLine("in list");
+            foreach (KeyValuePair<int, string> i in d)
+            {
+                Console.WriteLine($"{i.Key} - {i.Value}");
+            }
+            int k;
+            Console.Write("key=");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("list for key");
+            foreach (KeyValuePair<int, string> i in d)
+            {
+                if (i.Key <= k)
+                {
+                    Console.WriteLine($"{i.Key} - {i.Value}");
+                    d1.Add(i.Key, i.Value);
+                }
+                else
+                {
+                    d2.Add(i.Key, i.Value);
+                }
+            }
+            Console.WriteLine("out dic 1");
+            string jsond1 = JsonConvert.SerializeObject(d1, Formatting.Indented);
+            Console.WriteLine(jsond1);
+            Console.WriteLine("out dic 2");
+            string jsond2 = JsonConvert.SerializeObject(d2, Formatting.Indented);
+            Console.WriteLine(jsond2);
+        }
+    }
+}
